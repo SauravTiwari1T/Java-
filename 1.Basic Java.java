6f3262218699c48191a83c4d2dcaf3389f377efa
SauravTiwari1T <120175548+SauravTiwari1T@users.noreply.github.com>
2024-10-23 21:42:14 +0530

    Create 1.Basic Java.java

---
+++ 1.Basic Java.java
@@ -0,0 +1,55 @@
+name = "Saurav"
+Name = "Ram"
+print(name)
+print(Name)
+Saurav
+Ram
+Indentation
+## Uses indentation to define block of code 
+age = 32
+if age > 30:
+    print(age)
+# Below print is out side of if block    
+print(age)
+32
+32
+print statment
+print('Hello world')
+Hello world 
+Line Continuation
+# whit help of backsalsh(\) we can continue to next line
+total=102+2+3+4+5+89+\
+78+56
+print(total)
+339
+Multiple Statements on a single line
+# Example 
+x = 5
+y = 4 
+z = 5+4
+print(z)
+
+# 0r 
+x= 5; y=4 ; z = x+y; print(z)
+9
+9
+Variable assignment
+# in python we do not need to define data type 
+number = 5
+age = 45 
+name = 'Saurav'
+print(number, age , name)
+5 45 Saurav
+type(age)
+int
+type(name)
+str
+type(number)
+int
+Type Inference
+var = 10 
+print(var)
+var = "Saurav"
+print(var)
+10
+Saurav
